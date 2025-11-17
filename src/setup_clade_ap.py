4a6ac50e5812c67be0e7f2aca9ed3eddd3db5b88
nylander <Johan.Nylander@nrm.se>
2025-11-17 15:27:38 +0100

    remove debug printing

--- src/setup_clade_ap.py
+++ src/setup_clade_ap.py
@@ -92,17 +92,17 @@ if __name__ == "__main__":
 
     trn = tree_reader.read_tree_file_iter(tname).__next__().label
     ## DEBUG
-    print(f"DEBUG {__file__}: done with get_ncbi_tax_tree_no_species.py")
-    print(f"DEBUG {__file__}: trn: {trn}")
-    input(f"DEBUG {__file__}: Press Enter to continue...")
+    #print(f"DEBUG {__file__}: done with get_ncbi_tax_tree_no_species.py")
+    #print(f"DEBUG {__file__}: trn: {trn}")
+    #input(f"DEBUG {__file__}: Press Enter to continue...")
 
     # run make_dirs.py
     print(colored.yellow("MAKING DIRS IN"), dirl, colored.yellow(emoticons.get_ran_emot("excited")))
     cmd = py+" "+DI+"make_dirs.py "+tname+" "+dirl
     os.system(cmd)
     ## DEBUG
-    print(f"DEBUG {__file__}: done with make_dirs.py")
-    input(f"DEBUG {__file__}: Press Enter to continue...")
+    #print(f"DEBUG {__file__}: done with make_dirs.py")
+    #input(f"DEBUG {__file__}: Press Enter to continue...")
 
     # run populate_dirs_first.py
     print(colored.yellow("POPULATING DIRS"), dirl, colored.yellow(emoticons.get_ran_emot("excited")))
@@ -113,8 +113,8 @@ if __name__ == "__main__":
 
     os.system(cmd)
     ## DEBUG
-    print(f"DEBUG {__file__}: done with populate_dirs_first.py")
-    input(f"DEBUG {__file__}: Press Enter to continue...")
+    #print(f"DEBUG {__file__}: done with populate_dirs_first.py")
+    #input(f"DEBUG {__file__}: Press Enter to continue...")
 
     if os.path.isfile("log.md.gz"):
         os.remove("log.md.gz")
@@ -123,8 +123,8 @@ if __name__ == "__main__":
     cmd = py+" "+DI+"cluster_tree.py "+dirl+"/"+trn+"/ "+logfile
     os.system(cmd)
     ## DEBUG
-    print(f"DEBUG {__file__}: done with cluster_tree.py")
-    input(f"DEBUG {__file__}: Press Enter to continue...")
+    #print(f"DEBUG {__file__}: done with cluster_tree.py")
+    #input(f"DEBUG {__file__}: Press Enter to continue...")
 
     print(colored.blue("PYPHLAWD DONE "+emoticons.get_ran_emot("excited")))
     end = datetime.now()
